fe3525cf4c8b8fec8284a66a2f18cb26a586fa49
erem2k <nope@nohost.com>
2024-11-25 21:08:28 +0100

    characters - backpack config

---
+++ addons/characters/backpacks/include/combatBackpack/CfgVehicles.hpp
@@ -0,0 +1,16 @@
+class gm_ge_army_backpack_90_base;
+
+class gme_bw_b_combatBackpack_flke: gm_ge_army_backpack_90_base
+{
+    scope=2;
+    scopeArsenal=2;
+
+    author="erem2k";
+
+    displayName="Combat Backpack (5-Color, Early)";
+    picture="\gme\bw\main\data\ui\icon_gme_bw_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "\gme\bw\characters\backpacks\data\combatBackpack_flke_co.paa"
+    };
+};
---
+++ addons/characters/backpacks/include/infantryBackpack/CfgVehicles.hpp
@@ -0,0 +1,16 @@
+class gm_ge_army_backpack_80_oli_base;
+
+class gme_bw_b_infantryBackpack_flke: gm_ge_army_backpack_80_oli_base
+{
+    scope=2;
+    scopeArsenal=2;
+
+    author="erem2k";
+
+    displayName="Infantry Backpack (5-Color, Early)";
+    picture="\gme\bw\main\data\ui\icon_gme_bw_wip_ca.paa";
+
+    hiddenSelectionsTextures[] = {
+        "\gme\bw\characters\backpacks\data\infantryBackpack_flke_co.paa"
+    };
+};
