9dcb3421d6f1555146af32789c6ae04d82c41d28
erem2k <nope@nohost.com>
2024-12-24 19:01:56 +0100

    weapons - update optics config

--- addons/weapons/optics/config.hpp
+++ addons/weapons/optics/config.hpp
@@ -1,59 +1,115 @@
-class gme_bw_optics_at_hot_day_2x_base
-{
+class gm_opticsVehicles_base;
+class gm_opticsVehicles_ti_green_base;
+class gm_opticsVehicles_nv_base;
 
+class gme_bw_optics_at_hot_day_2x_base: gm_opticsVehicles_base
+{
+	gunneropticsModel="\gme\bw\weapons\optics\at_hot_day_2x.p3d";
+	visionMode[]= { "normal" };
+
+	initFov="0.4/2";
+	minFov="0.4/2";
+	maxFov="0.4/2";
 };
 
-class gme_bw_optics_at_hot_day_8x_base
+class gme_bw_optics_at_hot_day_8x_base: gm_opticsVehicles_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\at_hot_day_8x.p3d";
+	visionMode[]= { "normal" };
 
+	initFov="0.4/8";
+	minFov="0.4/8";
+	maxFov="0.4/8";
 };
 
-class gme_bw_optics_at_hot_ti_2x_base
+class gme_bw_optics_at_hot_ti_2x_base: gm_opticsVehicles_ti_green_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\at_hot_ti_2x.p3d";
 
+	initFov="0.4/2";
+	minFov="0.4/2";
+	maxFov="0.4/2";
 };
 
-class gme_bw_optics_at_hot_ti_8x_base
+class gme_bw_optics_at_hot_ti_8x_base: gm_opticsVehicles_ti_green_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\at_hot_ti_8x.p3d";
 
+	initFov="0.4/8";
+	minFov="0.4/8";
+	maxFov="0.4/8";
 };
 
-class gme_bw_optics_aa_sam_day_2x_base
+class gme_bw_optics_aa_sam_day_2x_base: gm_opticsVehicles_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\aa_sam_day_2x.p3d";
+	visionMode[]= { "normal" };
 
+	initFov="0.4/2";
+	minFov="0.4/2";
+	maxFov="0.4/2";
 };
 
-class gme_bw_optics_aa_sam_day_8x_base
+class gme_bw_optics_aa_sam_day_8x_base: gm_opticsVehicles_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\aa_sam_day_8x.p3d";
+	visionMode[]= { "normal" };
 
+	initFov="0.4/8";
+	minFov="0.4/8";
+	maxFov="0.4/8";
 };
 
-class gme_bw_optics_aa_sam_nv_2x_base
+class gme_bw_optics_aa_sam_nv_2x_base: gm_opticsVehicles_nv_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\aa_sam_nv_2x.p3d";
 
+	initFov="0.4/2";
+	minFov="0.4/2";
+	maxFov="0.4/2";
 };
 
-class gme_bw_optics_aa_sam_nv_8x_base
+class gme_bw_optics_aa_sam_nv_8x_base: gm_opticsVehicles_nv_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\aa_sam_nv_8x.p3d";
 
+	initFov="0.4/8";
+	minFov="0.4/8";
+	maxFov="0.4/8";
 };
 
-class gme_bw_optics_aa_mg_day_2x_base
+class gme_bw_optics_aa_mg_day_2x_base: gm_opticsVehicles_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\aa_mg_day_2x.p3d";
 
+	initFov="0.4/2";
+	minFov="0.4/2";
+	maxFov="0.4/2";
 };
 
-class gme_bw_optics_aa_mg_day_8x_base
+class gme_bw_optics_aa_mg_day_6x_base: gm_opticsVehicles_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\aa_mg_day_8x.p3d";
 
+	initFov="0.4/8";
+	minFov="0.4/8";
+	maxFov="0.4/8";
 };
 
-class gme_bw_optics_aa_mg_nv_2x_base
+class gme_bw_optics_aa_mg_nv_2x_base: gm_opticsVehicles_nv_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\aa_mg_nv_2x.p3d";
 
+	initFov="0.4/2";
+	minFov="0.4/2";
+	maxFov="0.4/2";
 };
 
-class gme_bw_optics_aa_mg_nv_8x_base
+class gme_bw_optics_aa_mg_nv_6x_base: gm_opticsVehicles_nv_base
 {
+	gunneropticsModel="\gme\bw\weapons\optics\aa_mg_nv_8x.p3d";
 
+	initFov="0.4/6";
+	minFov="0.4/6";
+	maxFov="0.4/6";
 };
